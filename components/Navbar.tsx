79df690153c5485ab5d1493ebba6a104766dc536
Hammami-Fedi <hammamifedi1997@gmail.com>
2024-03-08 13:48:13 +0100

    Style Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const session = null;
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={115} height={43} alt="Dribble" />
+        </Link>
+        <ul className=" xl:flex hidden text-sm gap-7">
+          {NavLinks.map((link) => (
+            <Link key={link.key} href={link.href}>
+              <li>{link.text}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
